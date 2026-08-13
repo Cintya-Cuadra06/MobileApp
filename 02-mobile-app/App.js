f7fe9e6f73c3f1e9923a67a1d16885728c1a3c37
Cintya Cuadra Salazar <cintyacuadrasalazar@gmail.com>
2026-08-13 15:51:05 -0600

    feat: add eslint

--- 02-mobile-app/App.js
+++ 02-mobile-app/App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, TouchableHighlight} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <StatusBar style="light" />
+
+      <TouchableHighlight
+      underlayColor="rgb(8, 42, 58)"
+      onPress={() => alert('Hola')}
+      style={{ backgroundColor: 'rgb(8, 42, 58)', width: 200, height: 50, 
+        alignItems: 'center', justifyContent: 'center', borderRadius: 100 }}
+      >
+        <Text style={{ color: 'white' }}>Pulsa aquí</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +21,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    marginTop: 10,
+    fontWeight: 'bold',
+  }
 });
